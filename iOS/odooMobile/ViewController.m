4588c0eb31364daad9fbad9b4628c44b2f5981c8
LeiQiao <lei.qiao@leiqiaodeMacBook-Pro.local>
2016-02-21 11:43:25 +0800

    create iOS Project
    创建iOS工程项目

---
+++ iOS/odooMobile/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  odooMobile
+//
+//  Created by lei.qiao on 16/2/19.
+//  Copyright © 2016年 LeiQiao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
