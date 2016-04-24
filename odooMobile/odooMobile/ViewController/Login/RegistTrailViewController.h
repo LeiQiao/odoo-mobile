89b9ffc7a390313088c49b7dc9aa2516d691e7e8
LeiQiao <leiqiaotalk@hotmail.com>
2016-04-24 14:17:48 +0800

    添加注释

--- odooMobile/odooMobile/ViewController/Login/RegistTrailViewController.h
+++ odooMobile/odooMobile/ViewController/Login/RegistTrailViewController.h
@@ -4,6 +4,10 @@
 
 #import <UIKit/UIKit.h>
 
+/*!
+ *  @author LeiQiao, 16/04/24
+ *  @brief 注册测试版窗体
+ */
 @interface RegistTrailViewController : UIViewController
 
 @end
--- odooMobile/odooMobile/ViewController/Login/RegistTrailViewController.m
+++ odooMobile/odooMobile/ViewController/Login/RegistTrailViewController.m
@@ -4,11 +4,20 @@
 
 #import "RegistTrailViewController.h"
 
+/*!
+ *  @author LeiQiao, 16/04/24
+ *  @brief 注册测试版窗体
+ */
 @implementation RegistTrailViewController
 
 #pragma mark
 #pragma mark button events
 
+/*!
+ *  @author LeiQiao, 16/04/24
+ *  @brief 发送邮件按钮按下
+ *  @param sender 发送邮件按钮
+ */
 -(IBAction) emailMe:(id)sender
 {
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"mailto://leiqiaotalk@hotmail.com"]];
