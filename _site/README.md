d9478465ddd75d211bd31825c1f9f0f8e05b4666
LeiQiao <lei.qiao@leiqiaodeMacBook-Pro.local>
2016-04-25 09:36:14 +0800

    添加登录功能

---
+++ _site/README.md
@@ -0,0 +1,3 @@
+# odoo-mobile
+odoo(OpenERP)的移动客户端<br/>
+包括Android和iOS版本（刚刚开始，毛也没有呢。。。）
---
+++ _site/iOS/odooMobile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  odooMobile
+//
+//  Created by lei.qiao on 16/2/19.
+//  Copyright © 2016年 LeiQiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
